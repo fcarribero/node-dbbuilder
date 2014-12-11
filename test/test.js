8f6c6c75a4dfbf37e021cbabf03d651db7b0a80c
fcarribero <francisco.carribero@gmail.com>
2014-12-11 10:56:11 -0300

    Removed test scripts

--- test/test.js
+++
@@ -1,7 +0,0 @@
-var argv = require('minimist')(process.argv.slice(2));
-var dbbuilder = require('../index');
-var fs = require('fs');
-
-dbbuilder.initialize(argv.h, argv.u, argv.p, argv.d, JSON.parse(fs.readFileSync('./struct.config.json')));
-
-dbbuilder.update();
